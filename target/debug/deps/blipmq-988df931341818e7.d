dcb8454b8b96cb023a15d327649733d7e69367f8
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-07-14 13:02:42 +0530

    Ignore unnecessary folder

--- target/debug/deps/blipmq-988df931341818e7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ashutosh Kumar\RustroverProjects\blipmq\target\debug\deps\blipmq-988df931341818e7.d: src\main.rs
-
-C:\Users\Ashutosh Kumar\RustroverProjects\blipmq\target\debug\deps\libblipmq-988df931341818e7.rmeta: src\main.rs
-
-src\main.rs:
--- target/debug/deps/blipmq-d582e5a187e0938e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ashutosh Kumar\RustroverProjects\blipmq\target\debug\deps\blipmq-d582e5a187e0938e.d: src\main.rs
-
-C:\Users\Ashutosh Kumar\RustroverProjects\blipmq\target\debug\deps\libblipmq-d582e5a187e0938e.rmeta: src\main.rs
-
-src\main.rs:
